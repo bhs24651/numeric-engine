623e4e45a8fe3d2dd65b2092af0eafa7b144df6d
Jason Wu <24651@burnside.school.nz>
2025-11-21 11:55:57 +1300

    Connected interactable elements in Settings to slots

--- settings.cpp
+++ settings.cpp
@@ -7,5 +7,19 @@ settings::settings(QWidget *parent)
 }
 
 settings::~settings()
-{}
+{
+	// QObject::connect(ui->wordlen_word, &QRadioButton::toggled, this, &MainWindow::on_wordlen_toggled);
+	QObject::connect(ui.auto_format, &QRadioButton::toggled, this, &settings::on_auto_format_toggled);
+	QObject::connect(ui.round_format, &QRadioButton::toggled, this, &settings::on_round_format_toggled);
+	QObject::connect(ui.scientific_format, &QRadioButton::toggled, this, &settings::on_scientific_format_toggled);
 
+	QObject::connect(ui.round_spinBox, &QSpinBox::valueChanged, this, &settings::on_round_spinBox_valueChanged);
+	QObject::connect(ui.scientific_spinBox, &QSpinBox::valueChanged, this, &settings::on_scientific_spinBox_valueChanged);
+}
+
+void settings::on_auto_format_toggled() {}
+void settings::on_round_format_toggled() {}
+void settings::on_scientific_format_toggled() {}
+
+void settings::on_round_spinBox_valueChanged() {}
+void settings::on_scientific_spinBox_valueChanged() {}
--- settings.h
+++ settings.h
@@ -11,6 +11,14 @@ public:
 	settings(QWidget *parent = nullptr);
 	~settings();
 
+private slots:
+	void on_auto_format_toggled();
+	void on_round_format_toggled();
+	void on_scientific_format_toggled();
+
+	void on_round_spinBox_valueChanged();
+	void on_scientific_spinBox_valueChanged();
+
 private:
 	Ui::settingsClass ui;
 };
