633a65233062cec203346869bbf45585969bb49d
Jason Wu <24651@burnside.school.nz>
2025-11-27 21:09:11 +1300

    Fixed Memory Leaks from About and Help forms

--- about.cpp
+++ about.cpp
@@ -4,6 +4,7 @@ about::about(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+	this->setAttribute(Qt::WA_DeleteOnClose);   // ADD THIS LINE
 }
 
 about::~about()
--- help.cpp
+++ help.cpp
@@ -4,6 +4,7 @@ help::help(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+	this->setAttribute(Qt::WA_DeleteOnClose);   // ADD THIS LINE
 }
 
 help::~help()
